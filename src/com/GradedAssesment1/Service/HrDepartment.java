c21dff2ab1ae051815d1b0924b0a254fc65f4b30
sindhu <279.sindhu@gmail.com>
2021-09-07 21:29:54 +0530

    First commit for Graded Assesment

---
+++ src/com/GradedAssesment1/Service/HrDepartment.java
@@ -0,0 +1,23 @@
+package com.GradedAssesment1.Service;
+
+public class HrDepartment extends SuperDepartment{
+	
+	public String departmentName()
+	{
+		return "Hr Department";
+	}
+	public String getTodaysWork()
+	{
+		return "Fill today’s worksheet and mark your attendance";
+	}
+	public String getWorkDeadline()
+	{
+		return "Complete by EOD";
+	}
+	public String doActivity()
+	{
+		return "team Lunch";
+	}
+
+
+}
---
+++ src/com/GradedAssesment1/main/Driver.java
@@ -0,0 +1,29 @@
+package com.GradedAssesment1.main;
+
+import com.GradedAssesment1.Service.AdminDepartment;
+import com.GradedAssesment1.Service.HrDepartment;
+import com.GradedAssesment1.Service.TechDepartment;
+
+public class Driver {
+
+	public static void main(String args[]) {
+		
+		AdminDepartment AD = new AdminDepartment();
+		System.out.println("Welcome to "+AD.departmentName());
+		System.out.println(AD.getTodaysWork());
+		System.out.println(AD.getWorkDeadline());
+		System.out.println(AD.isTodayAHoliday());
+		HrDepartment HD = new HrDepartment();
+		System.out.println("Welcome to "+HD.departmentName());
+		System.out.println(HD.doActivity());
+		System.out.println(HD.getTodaysWork());
+		System.out.println(HD.getWorkDeadline());
+		System.out.println(AD.isTodayAHoliday());
+		TechDepartment TD = new TechDepartment();
+		System.out.println("Welcome to "+TD.departmentName());
+		System.out.println(TD.getTodaysWork());
+		System.out.println(TD.getWorkDeadline());
+		System.out.println(TD.getTechStackInformation());
+		System.out.println(AD.isTodayAHoliday());
+	}	
+}
